d6f1f4ddfea073d979d6a3180ce0af66b8520ddd
tayoolaigbe <tayoolaigbe@outlook.com>
2023-09-10 23:36:18 +0100

    Modified the tailwind config and added my first component: Button

--- vidchill/src/pages/index.tsx
+++ vidchill/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
+import Button from "~/Components/Button";
 
 import { api } from "~/utils/api";
 
@@ -60,7 +61,7 @@ function AuthShowcase() {
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined }
+    { enabled: sessionData?.user !== undefined },
   );
 
   return (
@@ -69,12 +70,13 @@ function AuthShowcase() {
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
       </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+      <Button
+        variant="secondary-gray"
+        size="2xl"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}
-      </button>
+      </Button>
     </div>
   );
 }
