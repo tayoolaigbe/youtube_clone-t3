c6b8c67f80acbe5dd8e92d310f1e39d8c9395ffa
tayoolaigbe <tayoolaigbe@outlook.com>
2023-09-11 23:57:49 +0100

    Completed the Navbar and allowed images from clodinary

--- vidchill/src/Components/VideoComponent.tsx
+++ vidchill/src/Components/VideoComponent.tsx
@@ -8,11 +8,13 @@ export function UserImage({
   className?: string;
 }) {
   return (
-    <div className={`relative h-9 w-9 ${className}`}>
+    <div className={`relative h-10 w-10 ${className}`}>
       <Image
         src={image || "/profile.jpg"}
         alt=""
         className="fill absolute rounded-full"
+        width={40}
+        height={40}
       />
     </div>
   );
