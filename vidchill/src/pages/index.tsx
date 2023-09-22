a822da208b68a701bf98781b8903b68467a7200a
tayoolaigbe <tayoolaigbe@outlook.com>
2023-09-23 00:03:17 +0100

    Added the Error and Loading components

--- vidchill/src/pages/index.tsx
+++ vidchill/src/pages/index.tsx
@@ -1,13 +1,31 @@
+import { NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import Button from "~/Components/Buttons/Button";
 import { Navbar, Sidebar, Layout } from "~/Components/Component";
+import { ErrorMessage, LoadingMessage } from "~/Components/ErrorMessage";
 
 import { api } from "~/utils/api";
 
-export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+const Home: NextPage = () => {
+  const { data, isLoading, error } = api.video.getRandomVideos.useQuery(40);
+
+  const Error = () => {
+    if (isLoading) {
+      return <LoadingMessage />;
+    } else if (error ?? !data) {
+      return (
+        <ErrorMessage
+          icon="GreenPlay"
+          message="No Videos"
+          description="Sorry there are no videos with that title."
+        />
+      );
+    } else {
+      return <></>;
+    }
+  };
 
   return (
     <>
@@ -17,10 +35,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout closeSidebar={true}>
-        <p>This is from the homepage</p>
+        {!data ?? error ? (
+          <Error />
+        ) : (
+          <>
+            <p>Hello World</p>
+          </>
+        )}
       </Layout>
-      {/* <Navbar />
-      <Sidebar /> */}
     </>
   );
-}
+};
+
+export default Home;
