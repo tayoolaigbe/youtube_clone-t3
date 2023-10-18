47bab8c5a9cdc3864df5d631989a1a70225f0784
tayoolaigbe <tayoolaigbe@outlook.com>
2023-10-18 21:55:57 +0100

    Completed the VideoComponents component for the home page

--- vidchill/src/Components/Component.tsx
+++ vidchill/src/Components/Component.tsx
@@ -2,5 +2,5 @@ export { default as Navbar } from "./Navbar";
 export { default as Sidebar } from "./Sidebar";
 export { default as Layout } from "./Layout";
 export { default as Footer } from "./Footer";
-
-export { UserImage } from "./VideoComponent";
+export { Thumbnail } from "./Thumbnail";
+export { UserImage, MultiColumnVideo } from "./VideoComponent";
