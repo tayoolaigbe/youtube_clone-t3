b8e988fd6d1ec4f63b21f59e4e8afb0c3b1775c3
tayoolaigbe <tayoolaigbe@outlook.com>
2023-11-12 19:38:59 +0000

    Added the Profile page

---
+++ vidchill/src/pages/[userId]/ProfileVideos.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import Layout from "~/Components/Layout";
+
+const ProfileVideos: NextPage = () => {
+  return (
+    <>
+      <Layout>
+        <>
+          <p>This is Profile Following</p>
+        </>
+      </Layout>
+    </>
+  );
+};
+
+export default ProfileVideos;
