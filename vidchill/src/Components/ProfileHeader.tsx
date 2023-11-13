91f5e5464bc1b1ab550537160057a15a4fcf62e2
tayoolaigbe <tayoolaigbe@outlook.com>
2023-11-13 21:33:08 +0000

    Updated the user.ts router to accomodate ProfileHeader.tsx functionality

--- vidchill/src/Components/ProfileHeader.tsx
+++ vidchill/src/Components/ProfileHeader.tsx
@@ -1,5 +1,62 @@
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { ErrorMessage, LoadingMessage } from "~/Components/ErrorMessage";
+import { api } from "~/utils/api";
+
 const ProfileHeader = () => {
-  return <>Profile Header</>;
+  const router = useRouter();
+  const { userId } = router.query;
+  const { data: sessionData } = useSession();
+
+  if (userId === sessionData?.user.id) {
+    console.log("This is your profile");
+  }
+
+  const { data, isLoading, error } = api.user.getChannelById.useQuery({
+    id: userId as string,
+    viewerId: sessionData?.user?.id as string,
+  });
+
+  const channel = data?.user;
+  const viewer = data?.viewer;
+  const errorTypes = !channel || !viewer || error || !data;
+
+  const Error = () => {
+    if (isLoading) {
+      return <LoadingMessage />;
+    } else if (errorTypes) {
+      return (
+        <ErrorMessage
+          icon="GreenPeople"
+          message="Error loading Channel"
+          description="Sorry there are no videos with that title."
+        />
+      );
+    } else {
+      return <></>;
+    }
+  };
+  return (
+    <>
+      <Head>
+        <title>{channel?.name ? channel.name + " VidChill Channel" : ""}</title>
+        <meta name="description" content={channel?.description || ""} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {errorTypes ? (
+        <Error />
+      ) : (
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="!-mt-6 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+            <div className="flex">
+              <p>Hello Folks</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ProfileHeader;
--- vidchill/src/server/api/routers/user.ts
+++ vidchill/src/server/api/routers/user.ts
@@ -4,6 +4,47 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
+  getChannelById: publicProcedure
+    .input(z.object({ id: z.string(), viewerId: z.string().optional() }))
+    .query(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+      if (!user) {
+        throw new Error("User Not Found!");
+      }
+
+      const followers = await ctx.prisma.followEngagement.count({
+        where: {
+          followingId: user.id,
+        },
+      });
+
+      const followings = await ctx.prisma.followEngagement.count({
+        where: {
+          followerId: user.id,
+        },
+      });
+
+      let viewerHasFollowed = false;
+
+      const userWithEngagement = { ...user, followers, followings };
+      if (input.viewerId && input.viewerId !== "") {
+        viewerHasFollowed = !!(await ctx.prisma.followEngagement.findFirst({
+          where: {
+            followingId: user.id,
+            followerId: input.viewerId,
+          },
+        }));
+        const viewer = {
+          hasFollowed: viewerHasFollowed,
+        };
+        return { user: userWithEngagement, viewer };
+      }
+    }),
+
   addFollow: publicProcedure
     .input(z.object({ followerId: z.string(), followingId: z.string() }))
     .mutation(async ({ ctx, input }) => {
