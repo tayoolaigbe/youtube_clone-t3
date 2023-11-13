06ce726a0212526771142b3a154bcc8cf413bf80
tayoolaigbe <tayoolaigbe@outlook.com>
2023-11-13 22:23:24 +0000

    Completed the ProfileHeader description section

--- vidchill/src/Components/ProfileHeader.tsx
+++ vidchill/src/Components/ProfileHeader.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { ErrorMessage, LoadingMessage } from "~/Components/ErrorMessage";
 import { api } from "~/utils/api";
+import { UserImage } from "./Component";
+import Image from "next/image";
 
 const ProfileHeader = () => {
   const router = useRouter();
@@ -47,13 +49,40 @@ const ProfileHeader = () => {
       {errorTypes ? (
         <Error />
       ) : (
-        <div className="mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="!-mt-6 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
-            <div className="flex">
-              <p>Hello Folks</p>
+        <>
+          <Image
+            alt="error"
+            className="h-32 w-full object-cover lg:h-64"
+            width={1080}
+            height={128}
+            src={channel.backgroundImage ?? "/background.jpg"}
+          />
+          <div className="mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="!-mt-6 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+              <div className="flex">
+                <UserImage
+                  className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+                  image={channel.image ?? ""}
+                />
+              </div>
+              <div className=" mt-6 min-w-0 flex-1 md:block">
+                <h1 className="truncate text-2xl font-bold text-gray-900">
+                  {channel.name}
+                </h1>
+                <p className="text-regular text-gray-600">{channel.handle}</p>
+                <div className="mt-1 flex items-center text-xs">
+                  <p className="text-sm text-gray-600">
+                    {channel.followers} Followers
+                  </p>
+                  <li className="pl-2 text-sm text-gray-500"></li>
+                  <p className="text-sm text-gray-600">
+                    {channel.followings} Following
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </>
       )}
     </>
   );
