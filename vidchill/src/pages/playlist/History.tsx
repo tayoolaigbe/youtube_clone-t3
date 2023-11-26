e06bebe66a52798aefa28f61351fdf0015bb8013
tayoolaigbe <tayoolaigbe@outlook.com>
2023-11-26 14:09:13 +0000

    Added the History component for History page

---
+++ vidchill/src/pages/playlist/History.tsx
@@ -0,0 +1,76 @@
+import { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { title } from "process";
+import { LoadingMessage, ErrorMessage } from "~/Components/ErrorMessage";
+import Layout from "~/Components/Layout";
+import { PlaylistPage } from "~/Components/PlaylistComponent";
+import { api } from "~/utils/api";
+
+const History: NextPage = () => {
+  const { data: sessionData } = useSession();
+  const QueryTitle = "History" as string;
+  const { data, isLoading, error } = api.playlist.getPlaylistByTitle.useQuery({
+    title: QueryTitle,
+    userId: sessionData?.user.id as string,
+  });
+
+  const Error = () => {
+    if (isLoading) {
+      return <LoadingMessage />;
+    } else if (error ?? !data) {
+      return (
+        <ErrorMessage
+          icon="GreenPlay"
+          message="No current History"
+          description="Watch some Videos to add history."
+        />
+      );
+    } else {
+      return <></>;
+    }
+  };
+
+  const playlist = data?.playlist;
+
+  return (
+    <>
+      <Layout>
+        {!playlist ? (
+          <Error />
+        ) : (
+          <PlaylistPage
+            playlist={{
+              id: data.playlist?.id || "",
+              title: data.playlist?.title || "",
+              description: data.playlist?.description ?? "",
+              videoCount: data.videos.length || 0,
+              playlistThumbnail: data.videos[0]?.thumbnailUrl ?? "",
+              createdAt: data.playlist?.createdAt || new Date(),
+            }}
+            videos={data.videos.map((video) => ({
+              id: video?.id ?? "",
+              title: video?.title ?? "",
+              thumbnailUrl: video?.thumbnailUrl ?? "",
+              createdAt: video?.createdAt ?? new Date(),
+              views: video?.views || 0,
+            }))}
+            authors={data.authors.map((author) => ({
+              id: author?.id ?? "",
+              name: author?.name ?? "",
+              image: author?.image ?? "",
+            }))}
+            user={{
+              id: data.user?.id ?? "",
+              name: data.user?.name ?? "",
+              image: data.user?.image ?? "",
+              followers: data.user?.followers ?? 0,
+            }}
+          />
+        )}
+      </Layout>
+    </>
+  );
+};
+
+export default History;
--- vidchill/src/server/api/routers/playlist.ts
+++ vidchill/src/server/api/routers/playlist.ts
@@ -8,6 +8,93 @@ import {
 } from "~/server/api/trpc";
 
 export const playlistRouter = createTRPCRouter({
+  getPlaylistByTitle: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        userId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      let rawPlaylist = await ctx.prisma.playlist.findFirst({
+        where: {
+          title: input.title,
+          userId: input.userId,
+        },
+        include: {
+          user: true,
+          videos: {
+            include: {
+              video: {
+                include: {
+                  user: true,
+                },
+              },
+            },
+          },
+        },
+      });
+      if (rawPlaylist === null || rawPlaylist === undefined) {
+        rawPlaylist = await ctx.prisma.playlist.create({
+          data: {
+            title: input.title,
+            userId: input.userId,
+          },
+          include: {
+            user: true,
+            videos: {
+              include: {
+                video: {
+                  include: {
+                    user: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+      }
+
+      const followers = await ctx.prisma.followEngagement.count({
+        where: {
+          followingId: rawPlaylist.userId,
+        },
+      });
+
+      const userWithFollowers = { ...rawPlaylist.user, followers };
+
+      const videosWithUser = rawPlaylist.videos.map(({ video }) => ({
+        ...video,
+        author: video?.user,
+      }));
+
+      const videos = videosWithUser.map(({ author, ...video }) => video);
+      const users = videosWithUser.map(({ user }) => user);
+
+      const videosWithCounts = await Promise.all(
+        videos.map(async (video) => {
+          const views = await ctx.prisma.videoEngagement.count({
+            where: {
+              videoId: video.id,
+              engagementType: EngagementType.VIEW,
+            },
+          });
+          return {
+            ...video,
+            views,
+          };
+        }),
+      );
+
+      const { user, videos: rawVideos, ...playlistInfo } = rawPlaylist;
+
+      return {
+        playlist: playlistInfo,
+        videos: videosWithCounts,
+        authors: users,
+        user: userWithFollowers,
+      };
+    }),
   getPlaylistById: publicProcedure
     .input(z.string())
     .query(async ({ ctx, input }) => {
