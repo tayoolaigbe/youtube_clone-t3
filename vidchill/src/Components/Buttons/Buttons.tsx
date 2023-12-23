b91eee77f5a4037e75f811bf1da39a5322c15577
tayoolaigbe <tayoolaigbe@outlook.com>
2023-12-23 21:22:06 +0000

    Added the DeleteButton.tsx and working on the Dashboard.tsx component

--- vidchill/src/Components/Buttons/Buttons.tsx
+++ vidchill/src/Components/Buttons/Buttons.tsx
@@ -3,3 +3,5 @@ export { default as FollowButton } from "./FollowButton";
 export { default as LikeDislikeButton } from "./LikeDislikeButton";
 export { default as SaveButton } from "./SaveButton";
 export { default as AnnouncementButton } from "./AnnouncementButton";
+export { default as PublishedButton } from "./PublishedButton";
+export { default as DeleteButton } from "./DeleteButton";
